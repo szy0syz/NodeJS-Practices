40cbffee14ccaba0aae718e5a618c1cb295bf2bf
szy0syz <185505508@qq.com>
2016-06-08 14:19:56 +0800

    sws_01

--- LearnNodejsByProjects/01SimpleWebServer/app.js
+++ LearnNodejsByProjects/01SimpleWebServer/app.js
@@ -1,3 +1,24 @@
-var http = require('heet')
-var path = require('path')
-var fs = require('fs')
+//require modules
+var http = require('heet');
+var path = require('path');
+var fs = require('fs');
+var url = require('url');
+
+//Array of Mime Types
+var mimeTypes = {
+	"html":"text/html",
+	"jpeg":"image/jpeg",
+	"jpe":"image/jpeg",
+	"png":"image/png",
+	"js":"text/javascript",
+	"css":"text/css"
+};
+
+http.createServer(function(req,res){
+	var uri = url.parse(req.url).pathName; // ex:http://szy4.com/index.html,pathName='/index.html'
+	var fileName = path.join(prcess.cwd(),unescape(uri));
+	console.log('Loding ' + uri);
+	var stats;
+})
+
+
