dd56bc72a160b777838e7da3579d02234a020c71
JerryShi <szy0syz@gmail.com>
2016-06-13 16:38:23 +0800

    ooo

---
+++ mypractices/ZLIB/zlib02.js
@@ -0,0 +1,19 @@
+var zlib = require('zlib');
+
+const input = '...........szy.............';
+zlib.deflate(input, (err, buffer) => {
+  if (!err) {
+    console.log(buffer.toString('base64'));
+  } else {
+    // handle error
+  }
+});
+
+const buffer = new Buffer('eJzT04OD4qpKPWQAAFDlBbc=', 'base64');
+zlib.unzip(buffer, (err, buffer) => {
+  if (!err) {
+    console.log(buffer.toString());
+  } else {
+    // handle error
+  }
+});
