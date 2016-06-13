e17f29d05af89690de77edcde25041b2c35a2320
JerryShi <szy0syz@gmail.com>
2016-06-13 16:21:55 +0800

    yeah..

--- MyPractices/ZLIB/zlib01.js
+++ MyPractices/ZLIB/zlib01.js
@@ -1,1 +1,8 @@
+//使用zlib用gzip压缩123.txt成gz格式压缩包
 var zlib = require('zlib');
+const zgip = zlib.createGzip();
+const fs = require('fs');
+const inp = fs.createReadStream('123.txt');
+const out = fs.createWriteStream('123.txt.gz');
+
+inp.pipe(zgip).pipe(out);
