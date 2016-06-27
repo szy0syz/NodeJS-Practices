d19ac19aa089f7cc4941134f2d4e6e0ac7da4009
szy0syz <szy0syz@gmail.com>
2016-06-27 22:18:23 +0800

    setp05 nodemailer OK

--- LearnNodejsByProjects/02BasicExpressWebsite/szy_website/routes/contact.js
+++ LearnNodejsByProjects/02BasicExpressWebsite/szy_website/routes/contact.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('contact', { title: 'contact' });
+});
+
+router.post('/',function(req,res,next){
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth:{
+			user: 'szy0syz@gmail.com',
+			pass: '7y7szy7?'
+		}
+	});
+
+	mailOptions = {
+		from: 'szyQQ <185505508@qq.com>',
+		to: 'szy0syz@gmail.com',
+		subject: 'SZY_Website Submission',
+		text: 'You have a new Submission with the following details... Name: ' + req.body.name + ' Eamil: ' + req.body.email + ' Message: ' + req.body.message,
+		html: '<p>You got a new Submission with the following details</p><ul><li>Name: '+ req.body.name +'</li><li>Eamil: '+ req.body.email +'</li><li>Message: '+ req.body.message +'</li></ul>'
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+			res.redirect('/');
+		} else {
+			console.log('Message Sent: ' + info.response);
+			res.redirect('/')
+		}
+	});
+
 });
 
 module.exports = router;
