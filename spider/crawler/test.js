71a6d6dcf94484b10debfd744a40b885e69ccbfb
JerryShi <szy0syz@gmail.com>
2016-07-13 11:57:12 +0800

    some fixed

--- spider/crawler/test.js
+++ spider/crawler/test.js
@@ -2,19 +2,18 @@ var Crawler = require("crawler");
 var url = require('url');
 
 var c = new Crawler({
-    maxConnections : 10,
-    // This will be called for each crawled page
-    callback : function (error, result, $) {
-        // $ is Cheerio by default
-        //a lean implementation of core jQuery designed specifically for the server
-        $('a').each(function(index, a) {
-            var toQueueUrl = $(a).attr('href');
-            c.queue(toQueueUrl);
-        });
-    }
+    maxConnections : 10
+    // ,
+    // // This will be called for each crawled page
+    // callback : function (error, result, $) {
+    //     // $ is Cheerio by default
+    //     //a lean implementation of core jQuery designed specifically for the server
+    //     $('a').each(function(index, a) {
+    //         var toQueueUrl = $(a).attr('href');
+    //         c.queue(toQueueUrl);
+    //     });
+    // }
 });
-
-//c.queue('http://ynamp.com/news/');
 
 // Queue URLs with custom callbacks & parameters
 c.queue(
