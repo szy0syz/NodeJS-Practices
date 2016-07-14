9424dce7825b74f144b48afc6b196284248440e9
szy0syz <szy0syz@gmail.com>
2016-07-15 00:38:31 +0800

    yngp p02 OK

--- spider/crawler/app1.js
+++ spider/crawler/app1.js
@@ -1,12 +1,24 @@
 var Crawler = require("crawler");
 var url = require('url');
+var mongoose = require('mongoose');
+var config = require('./config');
+
+mongoose.connect('mongodb://localhost/yngp');
+var schema = new mongoose.Schema({
+    id: String,
+    title: String,
+    district: String,
+    publishDate: Date
+});
+
+var Bulletin = mongoose.model('bulletins', schema);
 
 var c = new Crawler({
     maxConnections : 10,
     forceUTF8 : true //转换到utf8格式
 });
 
-var config = require('./config');
+
 
 
 // Queue URLs with custom callbacks & parameters
@@ -44,18 +56,39 @@ c.queue(
 
         // filting   如果可以写入mongoDB
         resList.forEach(function(item,index){
-        	//console.log(item.title);
         	var assert = false;
-        	filterString.forEach(function(filter,f_index){   
+            // 循环过滤字符串数组中的字符串，只要包含其中一个字符串就assert:true
+        	config.filterString.forEach(function(filter,f_index){   
         		if(assert) {
-                    //写入mongoDB
-
+                    // 处理完后直接跳出该item的title循环检查，指向下一个。
                     return;   
                 }
-
+                // 检查tile是否包含关键字
         		if(item.title.search(filter) != -1){
         		console.log(item.title);
-        		assert = true;}
+        		assert = true; // 设置跳过这个item的检查
+                ////
+                //写入mongoDB
+                    Bulletin.findOne({'id': item.id},'title',function(err,finding_bul){ //其实这里的bul等于select id这个字段
+                        if(err) {
+                            console.log(err)
+                            return;
+                        }
+                        // 如果已存了这个公告则直接返回！
+                        if(finding_bul) return;
+                        // 数据库没存就存入mongoDB
+                        var bul = new Bulletin(item);
+                        bul.save(function(err){
+                            if(err) {
+                                console.log(err);
+                            } else {
+                                console.log("mongoose written 1 bulletin : ", item.title);
+                            }
+                        })
+                        //
+                    });
+                ////
+                }
         	})
 
         	
@@ -66,3 +99,6 @@ c.queue(
 );
 
 // http://www.yngp.com/bulletin_zz.do?method=showBulletin&bulletin_id=516af8c0.155d5b7c9a6.-725a
+// 优化查询完后为什么不退出，占用磁盘读写的问题！！！
+// 研究闭关mongoDB链接的问题
+// 研究crawler的细节问题，如执行后退出问题。
--- spider/crawler/config.js
+++ spider/crawler/config.js
@@ -1,7 +1,7 @@
 module.exports = 
 {
 	//标题过滤字符串[可以写regEX]
-	'filterString' : ['医院','福贡'],
+	'filterString' : ['医院','福贡','专科','学生'],
 	//采集的目标网址
 	'uri' : "http://www.yngp.com/bulletin.do?method=moreList&sign=0&districtCode=all"
 }
