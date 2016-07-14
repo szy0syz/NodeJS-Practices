0c773be356df37b7352fcec6feefef63ba56d5fb
JerryShi <szy0syz@gmail.com>
2016-07-14 13:20:33 +0800

    yngp p01 OK

--- spider/crawler/app1.js
+++ spider/crawler/app1.js
@@ -2,32 +2,59 @@ var Crawler = require("crawler");
 var url = require('url');
 
 var c = new Crawler({
-    maxConnections : 10
+    maxConnections : 10,
+    forceUTF8 : true //转换到utf8格式
 });
 
-// http://www.yngp.com/login.do?method=gotoCGGG
-// http://www.yngp.com/bulletin.do?method=moreList
+var filterString = ['医院','福贡'];
 
 // Queue URLs with custom callbacks & parameters
 c.queue(
     [{ 
-    //uri: 'http://www.yngp.com/login.do?method=gotoCGGG',
-    uri: 'http://www.yngp.com/bulletin.do?method=moreList',
+    uri: 'http://www.yngp.com/bulletin.do?method=moreList&sign=0&districtCode=all', //每次get请求有20条公告消息
     jQuery: true,
-    method: 'POST',
-    form: {key: 'value0', key2: 'value1'},
+
     // The global callback won't be called
     callback: function (error, result, $) {
-        // $('iframe').each(function(index, f) {
-        //     console.log($(f).attr('src'));
-        // });
+    	var resList = [];
+        $('#bulletinstable_table_body').find('tr').each(function(index,tr){
+        	var result = new Object();
+        	$(tr).find('td').each(function(index,td){
+        		if(index == 0){
+        			$(td).find('font').each(function(index,font){
+        				//console.log($(font).attr('title'),'\n',$(font).attr('value'),'\n');
+        				result.title = $(font).attr('title');
+        				result.value = $(font).attr('value');
+        			});
+        		}
+        		if(index == 1){
+        			result.district = $(td).html();
+        			//console.log($(td).html());
+        		}
+        		if(index == 2){
+        			result.publishDate = $(td).html();
+        			//console.log($(td).html());
+        		}
+        	});
+        	//console.log(result);
+        	resList.push(result)
+        });
+        //console.log('Body:\n', result.body);
 
-        console.log('Body:\n', result.body);
+        //console.log(resList);
+        resList.forEach(function(item,index){
+        	//console.log(item.title);
+        	var assert = false;
+        	filterString.forEach(function(filter,index){   
+        		if(assert) return;   		
+        		if(item.title.search(filter) != -1){
+        		console.log(item.title);
+        		assert = true;}
+        	})
+
+        	
+        });
+
     }
     }]
 );
-
-// http://www.yngp.com/bulletin.do?method=moreList&sign=0&disricetCode=all
-
-//post text view
-//ec_i=bulletinstable&bulletinstable_efn=&bulletinstable_crd=20&bulletinstable_p=1&bulletinstable_s_bulletintitle=&bulletinstable_s_codeName=&bulletinstable_s_finishday=&query_sign=1&listSign=1&findAjaxZoneAtClient=false&query_bulletintitle=&sign=0&disricetCode=all&flag=1&query_endTime=&query_startTime=&method=moreList&method=moreList&bulletinstable_totalpages=0&bulletinstable_totalrows=&bulletinstable_pg=1&bulletinstable_rd=20&districtCode=all
